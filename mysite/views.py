48679c4903c29278800f71151c1f9c79b5f7f5e9
YiChin <0123456ghjkl@gmail.com>
2022-03-09 14:13:36 +0800

    123

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,21 @@
+from cgitb import html
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
+#from .models import ToDoList, Item
+import datetime
 
-# Create your views here.
+def index(request):
+    html = "<h2>Hello</h2><hr>"
+    now = datetime.datetime.now()
+    html = html + "現在時刻:" + str(now)
+    return HttpResponse(html)
+ 
+def showdate(request):
+
+    now = datetime.datetime.now()
+    return HttpResponse("現在時刻:" + str(now))
+
+def index(response, id):
+    #Is = ToDoList.objects.get(id = id)
+    return render(response,"base.html",{})
