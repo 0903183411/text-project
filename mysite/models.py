c7775d1ee1004219c24b093e3ccd5bed9ea5929e
Richard <skynet.tw@gmail.com>
2022-03-14 23:04:26 +0800

    新增了一張資料表

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=10)  #股票名稱
+    sid = models.CharField(max_length=10)   #股票代號
+    price = models.FloatField(default=0.0)  #收盤價
+    date = models.DateField()               #日期
+
+    class Meta:
+        ordering = ('-date',)
+
+    def __str__(self):
+        return self.name
--- mysite/views.py
+++ mysite/views.py
@@ -2,20 +2,19 @@ from cgitb import html
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import ToDoList, Item
-import datetime
+import datetime, twstock
+from mysite.models import Stock
 
 def index(request):
-    html = "<h2>Hello</h2><hr>"
+    
     now = datetime.datetime.now()
-    html = html + "現在時刻:" + str(now)
-    return HttpResponse(html)
+    # stock = twstock.Stock('2330')
+    # prices = stock.price
+    # dates = stock.date
+    mystock = Stock.objects.all()
+    return render(request, "index.html", locals())
  
 def showdate(request):
 
     now = datetime.datetime.now()
     return HttpResponse("現在時刻:" + str(now))
-
-def index(response, id):
-    #Is = ToDoList.objects.get(id = id)
-    return render(response,"base.html",{})
